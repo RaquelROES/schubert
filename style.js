1c7d7e42d54ab2350dbc9bd50c62df94a93c59c4
RaquelROES <32135998+RaquelROES@users.noreply.github.com>
2020-12-10 16:11:15 +0100

    Update style.js

--- style.js
+++ style.js
@@ -18,9 +18,7 @@
 				{ name: 'Título 1', element: 'h4', attributes: { 'class': 'bck-title1'} },
 				{ name: 'Título 2', element: 'h4', attributes: { 'class': 'bck-title2'} },
 				{ name: 'Título 3', element: 'h4', attributes: { 'class': 'bck-title3'} },
-
-				{ name: 'Énfasis', element: 'span', attributes: { 'class': 'bck-enfasis' }},
-				{ name: 'Enunciado actividad', element: 'h4', attributes: { 'class': 'bck-title-activity' }},
+				
 
 				{ name: 'Tabla centrada', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table-center'} },
 				{ name: 'Celda encabezado', element: 'td', attributes: { 'class': 'bck-td' } },
